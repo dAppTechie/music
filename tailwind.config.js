d7af78d457bb1b25dd8a0fa848534513cbcee4f7
Chad Parker <63312621+ChimpBite@users.noreply.github.com>
2021-06-29 16:40:56 -0600

    styling done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
-  darkMode: true, // or 'media' or 'class'
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
