1c8e3efbb1cf3515b4f4fb219eb79fbb811ce7d8
Chad Parker <63312621+ChimpBite@users.noreply.github.com>
2021-06-29 16:16:19 -0600

    finished setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
+  darkMode: true, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
